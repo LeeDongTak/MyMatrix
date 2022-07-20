6edd505e0e72f31c2713e0365db5b40b88346184
LeeDongTak <sunny5796@naver.com>
2022-07-20 18:01:54 +0900

    로그인 페이지 회원가입 버튼 오류 수정

--- front/src/signin.js
+++ front/src/signin.js
@@ -12,12 +12,9 @@ function SignIn(){
                         <input type="text" placeholder="이메일" id="email" />
                         <input type="password" placeholder="비밀번호" id="password" />
                         <input type="button" value="로그인" id="signin" />
+                        <div className="signup_btn"><a href='#'>회원가입</a></div>
                     </div>
-                    {/* <!-- signin_form --> */}
-                    <div className="signup_container">
-                            <a href="#" id="sinup">회원가입</a>
-                    </div>
-                    {/* signup_container */}
+                    {/* signin_form */}
                 </div>
                 {/* signin_container */}
             </div>
